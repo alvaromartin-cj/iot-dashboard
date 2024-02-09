ae7dbe93d7b1e8a871d3912d5341b5f9208151bc
alvaromartin-cj <alvaromartincj@gmail.com>
2024-02-09 19:45:44 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'iot-dashboard',
+  base:'/iot-dashboard/',
   plugins: [
     vue(),
   ],
